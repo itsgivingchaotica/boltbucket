996e378e4b65d90920e2392656efc287e958ee52
Saoirse Siobhan Ebert <91578619+itsgivingchaotica@users.noreply.github.com>
2023-10-14 12:26:10 -0400

    export

--- server/controllers/customCars.js
+++ server/controllers/customCars.js
@@ -1,4 +1,4 @@
-import CustomCar from '../models/customCar';
+import CustomCar from "../models/customCar";
 
 //show all custom cars
 const index = async (req, res, next) => {
@@ -8,49 +8,72 @@ const index = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
 
 //show one custom car
 const show = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const customCar = await CustomCar.findOne(id);
-        res.status(200).json(customCar.rows[0]);
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const { id } = req.params;
+    const customCar = await CustomCar.findOne(id);
+    res.status(200).json(customCar.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+};
 
 //create a new custom car
 const create = async (req, res, next) => {
-    try {
-        const { carName, exterior, wheels, roof, interior, pricepoint } = req.body;
-        const customCar = await CustomCar.create(carName, exterior, wheels, roof, interior, pricepoint);
-        res.status(201).json(customCar.rows[0]);
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const { carName, exterior, wheels, roof, interior, pricepoint } = req.body;
+    const customCar = await CustomCar.create(
+      carName,
+      exterior,
+      wheels,
+      roof,
+      interior,
+      pricepoint
+    );
+    res.status(201).json(customCar.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+};
 
 //update an existing custom car
 const update = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const { carName, exterior, wheels, roof, interior, pricepoint } = req.body;
-        const customCar = await CustomCar.update(id, carName, exterior, wheels, roof, interior, pricepoint);
-        res.status(200).json(customCar.rows[0]);
-    } catch (error) {
-        next(error);
-    }
-}      
+  try {
+    const { id } = req.params;
+    const { carName, exterior, wheels, roof, interior, pricepoint } = req.body;
+    const customCar = await CustomCar.update(
+      id,
+      carName,
+      exterior,
+      wheels,
+      roof,
+      interior,
+      pricepoint
+    );
+    res.status(200).json(customCar.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+};
 
 //delete an existing custom car
 const deleteCustomCar = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        await CustomCar.delete(id);
-        res.sendStatus(204);
-    } catch (error) {
-        next(error);
-    }
-}
+  try {
+    const { id } = req.params;
+    await CustomCar.delete(id);
+    res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default {
+  index,
+  show,
+  create,
+  update,
+  delete: deleteCustomCar,
+};
